0585ea9afda2aa9aa8fd3789c4d2e4193b19f517
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-05-27 00:39:21 -0600

    refine card structure to address rendered markup warnings

--- app/src/index.js
+++ app/src/index.js
@@ -20,10 +20,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#1 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -34,10 +32,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#2 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -48,10 +44,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#3 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -62,10 +56,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#4 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -76,10 +68,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#5 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -89,9 +79,7 @@ const App = () => (
 					<Col xs={6} md={4} lg={2}>
 						<Card className="h-100 bg-warning">
 							<Card.Body className="justify-content-center align-items-center d-flex flex-column">
-								<Card.Title>
-									<h3>Rank Now!</h3>
-								</Card.Title>
+								<Card.Title>Rank Now!</Card.Title>
 								<Card.Text>Did we miss the best restaurant in town? Did you want to add a new restaurant?</Card.Text>
 								<Button variant="light">Submit a Restaurant</Button>
 							</Card.Body>
@@ -111,10 +99,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#1 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -125,10 +111,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#2 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -139,10 +123,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#3 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -153,10 +135,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#4 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -167,10 +147,8 @@ const App = () => (
 							<Card.Body>
 								<Card.Title>#5 Restaurant</Card.Title>
 								<hr/>
-								<Card.Text>
-									<h5>Restaurant Name</h5>
-									<p>Score</p>
-								</Card.Text>
+								<h5>Restaurant Name</h5>
+								<Card.Text>Score</Card.Text>
 								<Button variant="primary">Go somewhere</Button>
 							</Card.Body>
 						</Card>
@@ -180,9 +158,7 @@ const App = () => (
 					<Col xs={6} md={4} lg={2}>
 						<Card className="h-100 bg-warning">
 							<Card.Body className="justify-content-center align-items-center d-flex flex-column">
-								<Card.Title>
-									<h3>Vote Now!</h3>
-								</Card.Title>
+								<h3>Vote Now!</h3>
 								<Card.Text>Does your mac and cheese look better than the rest? Do you want to put your plate to the poll?</Card.Text>
 								<Button variant="light">Upload a Picture</Button>
 							</Card.Body>
